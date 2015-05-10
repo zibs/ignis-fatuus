d3777455df0a53c3554530f1e7981e9f08f5eb8d
zibs <elizibin@gmail.com>
2015-05-09 22:30:41 -0700

    Add flashing

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
   $('img').draggable();
 });
+
+$(document).ready(function() { 
+	$( "#forteff" ).click(function() {
+  	$( "img" ).toggle( "pulsate", {times:2},  500 )
+  			  .toggle( "pulsate", {times:2}, 1000);
+	});
+});
