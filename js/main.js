86b369ae3b5a99f2e30c93934bdea64a0ffe0278
zibs <elizibin@gmail.com>
2015-05-10 20:11:15 -0700

    Add images and adjust Jquery

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
-  $('img').draggable();
+  $('img').draggable()
+  	
 });
 
 $(document).ready(function() { 
 	$( "#forteff" ).click(function() {
-  	$( "img" ).toggle( "pulsate", {times:2},  500 )
-  			  .toggle( "pulsate", {times:2}, 1000);
+  	$( "img" ).effect( "pulsate", {times:2},  1000 )
+  			 
 	});
 });
